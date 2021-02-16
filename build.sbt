21df83b890746f25989ed9cc53f1d1ca08b797cc
Robin Yeh <robin.yeh@broadcom.com>
2021-02-16 23:02:50 +0800

    Create new scala project

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "TweetCleaning"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
+
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % "2.1.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+import org.apache.spark._
+//import org.apache.spark.streaming._
+//import org.apache.spark.streaming.kafka
+
+// References
+// Apache Spark & Kafka, Streaming Partners
+// https://medium.com/@gobiviswaml/apache-spark-kafka-streaming-partners-d492bffb8015
+
+// Twitter Sentiment Analysis
+// https://github.com/vspiewak/twitter-sentiment-analysis
+
+object Main extends App {
+  val conf = new SparkConf()
+  println("Hello, World from Robin!")
+}
