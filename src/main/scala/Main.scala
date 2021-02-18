3a844329a5032603e8611765547658e235f5b191
Robin Yeh <robin.yeh@broadcom.com>
2021-02-19 02:00:16 +0800

    Write cleaned tweets to another kafka topic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,41 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
 object Main extends App {
-  def tweetSchema(): StructType = {
+  val spark = SparkSession.builder.appName("TweetCleaning").master("local[*]").getOrCreate()
+  val df_raw = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "192.168.1.77:9092")
+    .option("subscribe", "twitter.raw")
+    .option("startingOffsets", "earliest")
+    .load()
+  df_raw.printSchema()
+
+  val df = df_raw.selectExpr("CAST(value AS STRING)") // cast value from bytes to string
+  val df_json = df.select(from_json(col("value"), Tweet.schema()).alias("parsed"))
+  df_json.printSchema()
+
+  val df_clean = df_json.select(
+    col("parsed.payload.Id").cast("string").alias("key"),
+    to_json(col("parsed.payload")).alias("value")
+  )
+    .writeStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "192.168.1.77:9092")
+    .option("topic", "twitter.clean")
+    .option("checkpointLocation", "/Users/ry735172/Projects/nus/TweetCleaning")
+    .start()
+  df_clean.awaitTermination()
+}
+
+object Tweet {
+  def schema(): StructType = {
     val userSchema = new StructType()
       .add("Id", LongType)
       .add("Name", StringType)
     val hashTagSchema = new StructType()
       .add("Text", StringType)
+
     new StructType()
       .add("payload", new StructType()
         .add("CreatedAt", LongType)
@@ -18,25 +47,5 @@ object Main extends App {
         .add("HashtagEntities", ArrayType(hashTagSchema))
       )
   }
-
-  val spark = SparkSession.builder.appName("TweetCleaning").master("local[*]").getOrCreate()
-  val df_kafka = spark
-    .readStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers", "192.168.1.77:9092")
-    .option("subscribe", "twitter.raw")
-    .option("startingOffsets", "earliest")
-    .load()
-  df_kafka.printSchema()
-
-  val df = df_kafka.selectExpr("CAST(value AS STRING)")
-  val df_json = df.select(from_json(col("value"), tweetSchema()).alias("parsed"))
-  val query = df_json.writeStream
-    .outputMode("append")
-    .format("console")
-    .option("truncate", "false")
-    .start()
-  df_json.printSchema()
-
-  query.awaitTermination()
 }
+
