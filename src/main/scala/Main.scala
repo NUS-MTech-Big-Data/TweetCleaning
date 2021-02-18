0925969663b067c086386852cfa5315e3e28277b
Robin Yeh <robin.yeh@broadcom.com>
2021-02-18 23:25:41 +0800

    Switch to use "structured streaming" instead of "spark streaming"
    https://blog.knoldus.com/spark-streaming-vs-structured-streaming/
    https://spark.apache.org/docs/2.2.0/structured-streaming-programming-guide.html

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,56 +1,25 @@
-import kafka.serializer.StringDecoder
-import org.apache.kafka.common.TopicPartition
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka
-import org.apache.spark.streaming.kafka.OffsetRange
-
-// References
-// Apache Spark & Kafka, Streaming Partners
-// https://medium.com/@gobiviswaml/apache-spark-kafka-streaming-partners-d492bffb8015
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
 
 // Twitter Sentiment Analysis
 // https://github.com/vspiewak/twitter-sentiment-analysis
 
-class SparkObjects() {
-
-  def GetSparkConf(): SparkConf ={
-    val conf = new SparkConf()
-    conf.setMaster("local[*]")
-    conf.setAppName("TweetCleaning")
-    conf
-  }
-
-  def GetStreamingContext(): StreamingContext ={
-    new StreamingContext(GetSparkConf(), Seconds(10))
-  }
-}
-
 object Main extends App {
   println("Hello, World from Robin!")
+  val spark = SparkSession.builder.appName("TweetCleaning").master("local[*]").getOrCreate()
+  val df_raw = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "192.168.1.77:9092")
+    .option("subscribe", "twitter.raw")
+    .option("startingOffsets", "earliest")
+    .load()
+  df_raw.printSchema()
+  val df = df_raw.selectExpr("CAST(value AS STRING)")
+  val query = df.writeStream
+    .outputMode("append")
+    .format("console")
+    .start()
 
-
-  val ssc = new SparkObjects().GetStreamingContext()
-  // Create direct kafka stream with brokers and topics
-  val topicsSet = Set("twitter.raw")
-  val kafkaParams = Map[String, String]("metadata.broker.list" -> "192.168.1.77:9092","auto.offset.reset" -> "smallest", "enable.auto.commit" -> "false")
-  val fromOffsets = Map(
-    new TopicPartition("twitter.raw", 0) -> 0
-  )
-
-  val kafkaStream = kafka.KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-
-  /*
-  val kafkaStream = kafka.KafkaUtils.createStream(ssc,
-    "192.168.1.77:2181","spark-streaming-consumer-group", Map("twitter.raw" -> 1) )
-    */
-  kafkaStream.print()
-  /*
-  kafkaStream.foreachRDD{(rdd, time) =>
-    rdd.collect().foreach(println)
-  }
-  */
-  kafkaStream.map(tuple => tuple._2).print()
-  ssc.start()
-  ssc.awaitTermination()
+  query.awaitTermination()
 }
