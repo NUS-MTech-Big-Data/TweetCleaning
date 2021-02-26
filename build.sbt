7de79c476baea47413491ed92d03f79fca7cc048
Robin Yeh <robin.yeh@broadcom.com>
2021-02-26 19:53:57 +0800

    Filter out non English tweets

--- build.sbt
+++ build.sbt
@@ -2,11 +2,14 @@ name := "TweetCleaning"
 
 version := "0.1"
 
-scalaVersion := "2.11.11"
+scalaVersion := "2.11.12"
 
 
-libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.0"
-libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
-libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.2.0"
-
+libraryDependencies ++= Seq(
+  "com.johnsnowlabs.nlp" %% "spark-nlp" % "2.7.4",
+  "org.apache.spark" %% "spark-mllib" % "2.4.7",
+  "org.apache.spark" %% "spark-core" % "2.4.7",
+  "org.apache.spark" %% "spark-sql" % "2.4.7",
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.7"
+)
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,21 @@
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types._
+import com.johnsnowlabs.nlp.pretrained.PretrainedPipeline
 
 object Main extends App {
+  def filterNonEnglish(df: DataFrame): DataFrame = {
+    val df_text = df_json.withColumn("text", col("parsed.payload.Text"))
+    val df_annotate_result = pipeline.annotate(df_text, "text")
+    val df_language = df_annotate_result.withColumn("language", explode(col("language.result")))
+    val df_english = df_language.filter(col("language") === "en")
+    df_english
+  }
+
+  val pipeline = new PretrainedPipeline("detect_language_43", lang = "xx")
   val spark = SparkSession.builder.appName("TweetCleaning").master("local[*]").getOrCreate()
+
+  // Read tweets from Kafka twitter.raw topic
   val df_raw = spark
     .readStream
     .format("kafka")
@@ -13,19 +25,22 @@ object Main extends App {
     .load()
   df_raw.printSchema()
 
+  // Parse tweets content from JSON bytes
   val df = df_raw.selectExpr("CAST(value AS STRING)") // cast value from bytes to string
   val df_json = df.select(from_json(col("value"), Tweet.schema()).alias("parsed"))
   df_json.printSchema()
 
-  val df_clean = df_json.select(
-    col("parsed.payload.Id").cast("string").alias("key"),
+  // Write cleaned tweets to Kafka twitter.clean topic
+  val df_clean = filterNonEnglish(df_json).select(
+    col("parsed.payload.Id").cast("string").alias("key"), // key must be string or bytes
     to_json(col("parsed.payload")).alias("value")
   )
     .writeStream
+    .outputMode("update")
     .format("kafka")
     .option("kafka.bootstrap.servers", "192.168.1.77:9092")
     .option("topic", "twitter.clean")
-    .option("checkpointLocation", "/Users/ry735172/Projects/nus/TweetCleaning")
+    .option("checkpointLocation", "/Users/ry735172/Projects/nus/TweetCleaning/write_checkpoint")
     .start()
   df_clean.awaitTermination()
 }
@@ -36,6 +51,7 @@ object Tweet {
       .add("Id", LongType)
       .add("Name", StringType)
       .add("ScreenName", StringType) // Twitter User Handle
+      .add("Location", StringType)
     val hashTagSchema = new StructType()
       .add("Text", StringType)
 
@@ -49,4 +65,3 @@ object Tweet {
       )
   }
 }
-
